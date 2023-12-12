698f304708ad42dadebe7f109b582e8172813803
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-12-12 17:54:02 +0600

    data fetch error. declare directly

--- src/component/sections/Skills.jsx
+++ src/component/sections/Skills.jsx
@@ -1,8 +1,48 @@
-import useSkills from "../../hooks/useSkills";
 import animation from '../../assets/animation.json'
 import Lottie from "lottie-react";
 const Skills = () => {
-    const [skills] = useSkills();
+    const skills = [
+        {
+          "id": 1,
+          "name": "React",
+          "image": "https://i.ibb.co/rkSv0sL/react.png"
+        },
+        {
+          "id": 2,
+          "name": "Tailwind",
+          "image": "https://i.ibb.co/kcsb21L/tailwind-removebg-preview.png"
+        },
+        {
+          "id": 3,
+          "name": "Next.js",
+          "image": "https://i.ibb.co/0DfQpcr/next-removebg-preview.png"
+        },
+        {
+          "id": 4,
+          "name": "React Query",
+          "image": "https://i.ibb.co/71KCBkB/react-query.png"
+        },
+        {
+          "id": 5,
+          "name": "JavaScript",
+          "image": "https://i.ibb.co/fCx7TWT/js.png"
+        },
+        {
+          "id": 6,
+          "name": "MongoDB",
+          "image": "https://i.ibb.co/g37NLsd/mongodb.png"
+        },
+        {
+          "id": 7,
+          "name": "Node.js",
+          "image": "https://i.ibb.co/MCzpbhY/node.png"
+        },
+        {
+          "id": 8,
+          "name": "Express",
+          "image": "https://i.ibb.co/jhn0hdV/express-js.png"
+        }
+      ]
 
     return (
         <div className=" bg-slate-100">
